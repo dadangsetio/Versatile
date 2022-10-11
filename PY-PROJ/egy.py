41a2a11205499c0c67e1a2b1f11a1a64594ec18d
Rohit Shaw <carpediem212045@gmail.com>
2022-10-11 14:02:04 +0530

    Fixed prolling issue

--- PY-PROJ/egy.py
+++ PY-PROJ/egy.py
@@ -5,6 +5,7 @@ class game_of_life:
 
         rows = len(board)
         cols = len(board[0])
+        
         copy_board = [[board[row][col] for col in range(cols)] for row in range(rows)]
 
          for row in range(rows):
@@ -16,10 +17,12 @@ class game_of_life:
 
                     r = (row + neighbor[0])
                     c = (col + neighbor[1])
+                    if (r < rows and r >= 0) and (c < cols and c >= 0) and (copy_board[r][c] == 1):
+                        live_neighbors += 1
      
                 if copy_board[row][col] == 1 and (live_neighbors < 2 or live_neighbors > 3):
                     board[row][col] = 0
-                # Rule 4
+             
                 if copy_board[row][col] == 0 and live_neighbors == 3:
                     board[row][col] = 1
        
